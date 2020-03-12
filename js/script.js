ac1e762e18b71602eb563a679090323667d63c40
Drew11 <drew.naumenko@gmail.com>
2020-03-12 10:19:52 +0200

    in progress

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const data = [
+const dataArr = [
     {
         "firstname":"Виктор",
         "lastname":"Петров",
@@ -30,62 +30,89 @@ const data = [
         "phonenumber":"33333333333",
         "birthday_contact":"1998-07-03",
         "company":"Company 4"
+    },
+
+    {
+        "firstname":"Змей",
+        "lastname":"Горыныч",
+        "email":"test@test.com",
+        "phonenumber":"33333333333",
+        "birthday_contact":"1998-05-03",
+        "company":"Company 4"
     }
 ];
 
-$('#example').DataTable({
-    data: data,
-    columns: [
-        { data: 'firstname'},
-        { data: 'lastname' },
-        { data: 'email' },
-        { data: 'phonenumber' },
-        { data: 'birthday_contact'},
-        { data: 'company'}
-    ],
-    columnDefs: [
-        {
-            targets: 0,
-            title: Object.keys(data[0])[0],
-        },
+let selectedQuery ='';
 
-    ]
-});
+function addEvent () {
+    const select = $('select[name="query-select"]');
 
-// function createThead () {
-//   const table = $('table');
-//   const captions  = Object.keys(data[0]);
-//   let count = 0;
-//
-//
-//   const thead = $('<thead></thead>');
-//   const tr = $('<tr></tr>');
-//
-//   while (count < captions.length){
-//       const th = $(`<th
-//                     class="sorting_asc"
-//                     tabindex="0"
-//                     aria-controls="example"
-//                     rowspan="1"
-//                     aria-sort="ascending"
-//                     aria-label=": activate to sort column descending"
-//                     >
-//
-//                    </th>`);
-//       th.text(`${captions[count]}`);
-//       tr.append(th);
-//       count++;
-//   }
-//
-//     console.log(table)
-//
-//   thead.append(tr);
-//   table.prepend(thead);
-// }
-//
-// createThead();
+    select.change(function (e) {
 
-$('#root').click(function () {
-    console.log('hi')
-});
+       const callbakcMap =  {
+            months:()=>{
+                table.destroy();
+                table = $('#example').DataTable(sortedByMonths(dataArr))
+            },
+            year: ()=>{
+                table.destroy();
+                table = $('#example').DataTable(sortedByYear(dataArr));
+            }
+        };
 
+        callbakcMap[e.target.value]();
+    });
+}
+
+
+function sortedByYear(data){
+    function f(obj) {
+        const index = obj.birthday_contact.indexOf('-');
+        const year = obj.birthday_contact.slice(0, index);
+        return year;
+    }
+    const sorted = [...data].sort((a, b)=> f(b) - f(a) );
+    return setDataToTable(sorted);
+}
+
+function sortedByMonths(data) {
+
+    function f(a) {
+        const index = a.birthday_contact.indexOf('-');
+        const secondIndex = a.birthday_contact.slice(index+1, a.birthday_contact.length).indexOf('-');
+        const d = a.birthday_contact.slice(index+1, a.birthday_contact.length);
+        const months = d.slice(0, secondIndex);
+        return months;
+    }
+    const sorted = [...data].sort((a, b)=>f(b) - f(a));
+    return setDataToTable(sorted);
+}
+
+
+function setDataToTable(data){
+    const columns = [];
+    const columnDefs = [];
+    const captions = Object.keys(data[0]);
+
+    captions.forEach((caption, index)=>{
+        columns.push({
+            data: caption
+        });
+        columnDefs.push({
+            targets: index,
+            title: captions[index]
+        });
+
+    });
+
+    return {
+        data,
+        columns,
+        columnDefs,
+        paging: false,
+        bSort: false,
+    }
+}
+addEvent();
+
+let table = $('#example').DataTable(setDataToTable(dataArr));
